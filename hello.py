8e5117d910f082d8ad4a7b5842f5582210b8f892
BEUTControls <62080432+BEUTControls@users.noreply.github.com>
2020-03-11 20:05:38 -0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,27 @@
+from time import sleep #This will allow us to use a pause in program
+#
+# If you are totally new to programming.
+# the # allows you to enter a comment in python.
+#
+# Just so we are clear. I have absolutely no musical talent.
+# Just wanted to spice up the hello-world.
+# Play around with the timing. 
+def hellofunc(): # This is a function. (i think). Not really need for a simple program like this.
+  # Code after a def needs to be indented.
+  # Adjust the sleep times to correct program to the song timing.
+  sleep(1)
+  print ("Hello")
+  sleep(1.5)
+  print ("Is)
+  sleep(0.5)
+  print("it")
+  sleep(0.5)
+  print("me")
+  sleep(0.75)
+  print("you're")
+  sleep(0.75)
+  print("looking")
+  sleep(1)
+  print("for")
+# The HELLOFUNC() calls the above code.
+hellofunc()
