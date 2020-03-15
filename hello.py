fe88b68408296d1e2298c1894c19c85698a5009a
BEUTControls <62080432+BEUTControls@users.noreply.github.com>
2020-03-15 06:00:46 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,9 +6,9 @@ from time import sleep #This will allow us to use a pause in program
 # Just so we are clear. I have absolutely no musical talent.
 # Just wanted to spice up the hello-world.
 # Play around with the timing. 
-def hellofunc(): # This is a function. (i think). Not really need for a simple program like this.
+def hellofunc(): # This is a function. (i think). No really need for a simple program like this.
   # Code after a def needs to be indented.
-  # Adjust the sleep times to correct program to the song timing.
+  # Adjust the sleep times to try and get the program timed right for the song.
   sleep(1)
   print ("Hello")
   sleep(1.5)
